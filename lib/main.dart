d4fead46f9b9f2aaef22341b0880ba9371cbf6ea
AMuellerAtAHS <amueller@ahs-prueftechnik.de>
2022-09-07 10:58:11 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,42 +33,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> releases = [];
+  String? latestRelease;
+  String currentRelease = "v0.0.2-alpha";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Center(
-        child: Text("Test"),
+      body: ListView.builder(
+        itemCount: releases.length,
+        itemBuilder: (context, index) => ListTile(
+          leading: latestRelease != null &&
+                  latestRelease != currentRelease &&
+                  latestRelease == releases[index]
+              ? Icon(Icons.system_update_alt)
+              : SizedBox(),
+          title: Text(
+            "${releases[index]}${releases[index] == currentRelease ? " - Installed" : ""}",
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final headers = {"Accept": "application/vnd.github+json"};
-          final uri = Uri.https(
-            "api.github.com",
-            "/repos/amuelleratahs/flutter_install_update_web/releases",
-          );
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              final headers = {"Accept": "application/vnd.github+json"};
+              final uri = Uri.https(
+                "api.github.com",
+                "/repos/amuelleratahs/flutter_install_update_web/releases",
+              );
 
-          try {
-            final res = await http.get(uri, headers: headers);
-            print(res.statusCode);
-            print(res.body);
-            if (res.statusCode == 200) {
-              if (res.body.isNotEmpty) {
-                final data = jsonDecode(res.body);
-                print(data);
-                for (var item in data) {
-                  print(item["name"]);
+              try {
+                final res = await http.get(uri, headers: headers);
+                print(res.statusCode);
+                print(res.body);
+                if (res.statusCode == 200) {
+                  if (res.body.isNotEmpty) {
+                    final data = jsonDecode(res.body);
+                    print(data);
+                    setState(() {
+                      releases =
+                          List.from((data as List).map((e) => e["name"]));
+                    });
+                    for (var item in releases) {
+                      print(item);
+                    }
+                    setState(() {
+                      latestRelease = releases.first;
+                    });
+                  }
                 }
+              } catch (e) {
+                print(e);
               }
-            }
-          } catch (e) {
-            print(e);
-          }
-        },
-        tooltip: 'Get Releases',
-        child: const Icon(Icons.add),
+            },
+            child: Text("Get releases"),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final headers = {"Accept": "application/vnd.github+json"};
+              final uri = Uri.https(
+                "api.github.com",
+                "/repos/amuelleratahs/flutter_install_update_web/releases/latest",
+              );
+
+              try {
+                final res = await http.get(uri, headers: headers);
+                print(res.statusCode);
+                print(res.body);
+                if (res.statusCode == 200) {
+                  if (res.body.isNotEmpty) {
+                    final data = jsonDecode(res.body);
+                    print(data);
+                    setState(() {
+                      latestRelease = data["name"];
+                    });
+                  }
+                }
+              } catch (e) {
+                print(e);
+              }
+            },
+            child: Text("Get latest release"),
+          ),
+        ],
       ),
     );
   }
